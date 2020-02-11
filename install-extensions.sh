c7a68626e7f7909fc31898edc68b17a788eb6f0d
Elan Ruusamäe <glen@pld-linux.org>
2020-02-11 02:19:10 +0200

    Install tideways extension for 5.x

--- install-extensions.sh
+++ install-extensions.sh
@@ -7,6 +7,22 @@ set -xeu
 : ${TIDEWAYS_XHPROF_VERSION=5.0.2}
 : ${PHP_VERSION=${TRAVIS_PHP_VERSION}}
 
+install_tideways() {
+	local version=$TIDEWAYS_VERSION
+	local url=https://github.com/tideways/php-profiler-extension.git
+
+	git clone -b v$version "$url" tideways
+	cd tideways
+	phpize
+	./configure
+	make
+	make install
+	cd ..
+
+	config="$HOME/.phpenv/versions/$PHP_VERSION/etc/tideways.ini"
+	echo "extension=tideways.so" > "$config"
+}
+
 install_tideways_xhprof() {
 	local version=$TIDEWAYS_XHPROF_VERSION
 	local url="https://github.com/tideways/php-xhprof-extension/releases/download/v$version/tideways-xhprof-$version-x86_64.tar.gz"
@@ -22,6 +38,9 @@ install_tideways_xhprof() {
 }
 
 case "$PHP_VERSION" in
+5.*)
+	install_tideways
+	;;
 7.*)
 	install_tideways_xhprof
 	;;
